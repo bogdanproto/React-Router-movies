70a1331ca3ca1dc4f234f4c1a8918aabab206de5
Bogdan Protasenko <b.protasenko@gmail.com>
2024-01-17 01:37:00 +0100

    test1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -15,7 +15,7 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
-        <Route path="movies" element={<Movies />} />
+        <Route path="moviesw" element={<Movies />} />
         <Route path="movies/:id" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
